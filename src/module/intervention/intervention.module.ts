f90250d822e446b9866ea37a732a9f68b03aa08c
Hammad153 <hammadismail1546@gmail.com>
2026-08-23 21:24:17 +0100

    feat: add CoachService to InterventionModule providers

--- src/module/intervention/intervention.module.ts
+++ src/module/intervention/intervention.module.ts
@@ -4,10 +4,11 @@ import { AiProviderModule } from '../../core/ai/ai-provider.module';
 import { DatabaseService } from '../../core/database/database.service';
 import { InterventionController } from './intervention.controller';
 import { InterventionService } from './intervention.service';
+import { CoachService } from './coach.service';
 
 @Module({
   imports: [AnalyticsModule, AiProviderModule],
   controllers: [InterventionController],
-  providers: [InterventionService, DatabaseService],
+  providers: [InterventionService, DatabaseService, CoachService],
 })
 export class InterventionModule {}
