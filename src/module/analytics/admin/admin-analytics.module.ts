9bbb5b7ba3398fc2c71eefd54939593e9441d427
Hammad153 <hammadismail1546@gmail.com>
2026-08-24 14:00:11 +0100

    feat: inject BehavioralEventService into AdminAnalyticsModule providers

--- src/module/analytics/admin/admin-analytics.module.ts
+++ src/module/analytics/admin/admin-analytics.module.ts
@@ -4,10 +4,11 @@ import { DatabaseService } from '../../../core/database/database.service';
 import { AdminAnalyticsController } from './admin-analytics.controller';
 import { AdminAnalyticsService } from './admin-analytics.service';
 import { AdminDashboardService } from './admin-dashboard.service';
+import { BehavioralEventService } from '../behavioral-event.service';
 
 @Module({
   imports: [AiProviderModule],
   controllers: [AdminAnalyticsController],
-  providers: [AdminAnalyticsService, AdminDashboardService, DatabaseService],
+  providers: [AdminAnalyticsService, AdminDashboardService, DatabaseService, BehavioralEventService],
 })
 export class AdminAnalyticsModule {}
